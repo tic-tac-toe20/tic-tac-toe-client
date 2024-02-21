aba7066fc4512a2c55a5c02f044695ff71364213
sultan <sultannursalimm@gmail.com>
2024-02-21 22:54:19 +0700

    feat: ngedit home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -159,9 +159,9 @@ export default function Home() {
       <button onClick={handlerLogOut}>log out</button>
       <div className="main-div">
         <div>
-          <div className="move-detection">
-            <div className="left">Kamu</div>
-            <div className="right">Lawan</div>
+        <div className="move-detection">
+            <div className="left">{localStorage.username}</div>
+            <div className="right">jodoh 💕</div>
           </div>
           <div>
             <h1 className="tittle-head transparant-div">Tic X Tac O Toe</h1>
@@ -174,6 +174,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+        
       </div>
     </>
   );
