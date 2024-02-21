36f6d4b2836f5940d2f581d34719c64e6a3f8800
sultan <sultannursalimm@gmail.com>
2024-02-21 10:02:56 +0700

    feat: home app.css index.css

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import Square from "../components/Square"
+import "../App.css"
+
+const renderForm = [[1,2,3],[4,5,6],[7,8,9]]
+
 export default function Home(){
     return(
         <>
+            <div className="main-div">
+        <div>
+        <div className="move-detection">
+            <div className="left">Kamu</div>
+            <div className="right">Lawan</div>
+          </div>
+          <div>
+            <h1 className="tittle-head transparant-div" >Tic X Tac O Toe</h1>
+            <div className="square-wrap">
+              {
+                renderForm.map((x)=>
+                x.map((a)=>{
+                  return <Square/>
+                }))
+              }
+            </div>
+        </div>
+        </div>
         
+      </div>
         </>
     )
 }
