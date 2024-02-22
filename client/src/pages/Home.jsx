1438b50f7c16a1c47e58e99de03dd86db669763a
deshinta <deshintaaulia065@gmail.com>
2024-02-22 18:05:48 +0700

    feat: debug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,6 @@ export default function Home() {
   const navigate = useNavigate();
   const [username, setUsername] = useState(localStorage.username);
   const [audio, setAudio] = useState(false); // unt audio
-  const [image, setImage] = useState(false);
   const [players, setPlayers] = useState([]);
   const [user, setUser] = useState({});
   const [click, setClick] = useState({
@@ -31,7 +30,6 @@ export default function Home() {
     i: 'i',
   });
   
-  
   if (
     (click.a == click.b && click.b == click.c) ||
     (click.d == click.e && click.e == click.f) ||
@@ -41,28 +39,9 @@ export default function Home() {
     (click.c == click.f && click.f == click.i) ||
     (click.a == click.e && click.e == click.i) ||
     (click.c == click.e && click.e == click.g)
-  ) {
-    console.log('game over jln', localStorage.username);
-    socket.emit('gameOver', localStorage.username);
-
-    // socket.on('gameOver', (e) => {
-    //    console.log(e, '<--- ini harusnya user yg won');
-    //     console.log(localStorage.username,'<- ini di kondisi game over', e,'-< yg menang');
-
-    //     if(localStorage.username === e){
-    //         console.log(localStorage.username, '<-username', e,'<-yg menang');
-    //         Swal.fire("You win!")
-    //     } else if (localStorage.username !== e) {
-    //         console.log(localStorage.username, '<-username', e,'<-yg menang');
-    //         Swal.fire("you lose!")
-    //     } 
-    //     // setTimeout(()=>{
-    //     //     localStorage.clear()
-    //     //     navigate('/login')
-    //     // },900)
-    // });
-  } else if (click.a!=='a' && click.b!=='b' && click.c!=='c' && click.d!=='d' && click.e!=='e' && click.f!=='f' && click.g!=='g' && click.h!=='h' && click.i!=='i') {
-        console.log('permainan draw!!!');
+    ) {
+        socket.emit('gameOver', localStorage.username);
+    } else if (click.a!=='a' && click.b!=='b' && click.c!=='c' && click.d!=='d' && click.e!=='e' && click.f!=='f' && click.g!=='g' && click.h!=='h' && click.i!=='i') {
         Swal.fire("draw!!!");
         setTimeout(()=>{
             localStorage.clear()
@@ -70,47 +49,33 @@ export default function Home() {
         },900)
     }
 
+
   function handlerLogOut() {
     localStorage.removeItem('username');
     navigate('/login');
   }
 
-  function imgPopup() {
-    setTimeout(() => {
-      console.log('awal jln');
-      setImage(true);
-      setTimeout(() => {
-        console.log('setelah jalan');
-        setImage(false);
-      }, 2000);
-    });
-  }
 
   function handlerAudio() {
     setTimeout(() => {
-      console.log('awal jln');
       setAudio(true);
       setTimeout(() => {
-        console.log('setelah jalan');
         setAudio(false);
       }, 1000);
     });
   }
 
   function handlerClick(key) {
+
     if (username === localStorage.username) {
-        console.log(localStorage.username,'<-username ', username);
-        console.log(players, '<- semua players');
       handlerAudio();
-    //   console.log({ ...click, [key]: user.symbol },'<-click');
       socket.emit('playing', { ...click, [key]: user.symbol }); // <<< 2 ngirim
+
       let userLogin = players.filter((el) => el.user !== localStorage.username);
-      console.log(userLogin[0],'<-==--=');
       socket.emit('username', userLogin[0].user);
     } else {
         handlerAudio();
         Swal.fire("bukan giliranmu!!!");
-      console.log('bukan giliranmu!!!');
     }
   }
 
@@ -123,9 +88,8 @@ export default function Home() {
     socket.connect();
 
     socket.on('find', (player) => {
-      console.log(player, '<=data players');
       setPlayers(player);
-      console.log(localStorage.username, '<- localstorage', player);
+      
       let userLogin = player.filter((el) => el.user === localStorage.username);
       setUser(userLogin[0]);
     });
@@ -133,6 +97,7 @@ export default function Home() {
     socket.on('playing', (e) => {
       // <<< 5 diterima cick barunya
       setClick(e); // <<< 6 di set
+      
     });
 
     socket.on('username', (e) => {
@@ -140,21 +105,18 @@ export default function Home() {
     });
 
     socket.on('gameOver', (e) => {
-           console.log(e, '<--- ini harusnya user yg won');
-            console.log(localStorage.username,'<- ini di kondisi game over', e,'-< yg menang');
     
-            if(localStorage.username === e){
-                console.log(localStorage.username, '<-username', e,'<-yg menang');
-                Swal.fire("You win!")
-            } else if (localStorage.username !== e) {
-                console.log(localStorage.username, '<-username', e,'<-yg menang');
-                Swal.fire("you lose!")
-            } 
-            // setTimeout(()=>{
-            //     localStorage.clear()
-            //     navigate('/login')
-            // },900)
-        });
+        if(localStorage.username === e){
+            Swal.fire("You win!")
+            // localStorage.clear()
+            // navigate('/login')
+        } else if (localStorage.username !== e) {
+            Swal.fire("you lose!")
+            // localStorage.clear()
+            // navigate('/login')
+        }
+
+    });
 
     return () => {
       socket.off('players:online');
@@ -167,13 +129,6 @@ export default function Home() {
     };
   }, []);
 
-  useEffect(() => {
-    console.log(user, '<-data user');
-  }, [user]);
-
-  useEffect(() => {
-    console.log(click, '<=');
-  }, [click]); // <<< ketriger step 1
 
   return (
     <>
@@ -183,7 +138,7 @@ export default function Home() {
           <source src="bell.wav" type="audio/mpeg" />
         </audio>
       )}
-      {image && <img src="/utta.jpg" alt="" style={{ position: 'absolute', top: '15%', left: '28%' }} />}
+      {/* {image && <img src="/utta.jpg" alt="" style={{ position: 'absolute', top: '15%', left: '28%' }} />} */}
       <button className='logOut' onClick={handlerLogOut}>log out</button>
       <div className="main-div">
         <div>
